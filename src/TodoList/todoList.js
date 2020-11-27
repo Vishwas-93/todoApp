98f6a2b99fc9ebb9ae5d609f791356bead1a7a8e
VishManja <vishwas.vasudevamanja@gmail.com>
2020-11-26 23:46:13 -0800

    Initial Commit

---
+++ src/TodoList/todoList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Todo from '../Todo/todo';
+
+const TodoList = () => {
+    return (
+        <div>
+            <Todo/>
+            <Todo/>
+            <Todo/>
+        </div>
+    )
+}
+
+export default TodoList;
